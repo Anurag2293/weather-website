7b3eaf38e131e6dc0ff66ab501bfa1ddf358ee13
Anurag Dhote <anuragdhote392@gmail.com>
2022-11-11 13:03:54 +0530

    7th Commit : Adding query String options to the browser url basic

--- src/app.js
+++ src/app.js
@@ -45,10 +45,30 @@ app.get('/help', (req, res) => {
 })
 
 app.get('/weather', (req, res) => {
+    
+    if (!req.query.address) {
+        return res.send({
+            error : 'You must provide an address'
+        })
+    }
+
     res.send({
-        city : 'Philadelphia',
-        latitude : 21.1,
-        longitude : -120
+        address : req.query.address
+    })
+})
+
+app.get('/products', (req, res) => {
+
+    if (!req.query.search) {
+        return res.send({
+            error : 'You must provide a search term'
+        })
+    }
+
+
+    console.log(req.query.search)
+    res.send({
+        products : []
     })
 })
 
